41c2659d02fc99f8732eaf9e7ffebcfadbb9b229
anilvibgyorweb <anil.vibgyorweb@gmail.com>
2025-04-07 11:44:15 +0530

    gshdhjgsd

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:4000/api/v1";
+export const API_URL = "https://sumaya-mlm-server.onrender.com/api/v1";
 // export const API_URL = "http://localhost:4001/api/v1";
 // export const API_URL = "https://funding-fettle-server.onrender.com/api/v1";
-export const IMG_URL = "https://api.jeevanincome.com";
+export const IMG_URL = "https://sumaya-mlm-server.onrender.com/";
